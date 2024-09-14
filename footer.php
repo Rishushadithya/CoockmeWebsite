3f82284c2921cbdb9b9a7b87754bab2b28f589dd
Rishushadithya <rishushadithya778@gmail.com>
2024-09-14 22:41:59 +0530

    head and footer upade

---
+++ footer.php
@@ -0,0 +1,71 @@
+<?php
+// session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>footer</title>
+</head>
+
+<body>
+    <footer class="site-footer">
+        <div class="footer-content">
+            <div class="logo-container">
+                <div class="flogo" id="flogo">
+                    <img src="./image/logo.jpg" alt="Logo">
+                </div>
+            </div>
+            <div class="footer-links">
+                <div class="category-links">
+                    <h3>Category</h3>
+                    <br>
+                    <div class="category-columns">
+                        <ul>
+                            <li><a href="#">Category 1</a></li>
+                            <li><a href="#">Category 2</a></li>
+                            <li><a href="#">Category 3</a></li>
+                            <li><a href="#">Category 4</a></li>
+                        </ul>
+                        <ul>
+                            <li><a href="#">Category 5</a></li>
+                            <li><a href="#">Category 6</a></li>
+                            <li><a href="#">Category 7</a></li>
+                            <li><a href="#">Category 8</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="quick-links">
+                    <h3>Quick Links</h3>
+                    <br>
+                    <ul>
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="about.php">About</a></li>
+                        <li><a href="contact.php">Contact</a></li>
+                        <li><a href="categories.php">Category</a></li>
+                    </ul>
+                </div>
+                <div class="footer-section social">
+                    <h3>Follow Us</h3>
+                    <br>
+                    <a href="#"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                    <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                </div>
+            </div>
+            
+        </div>
+        <div class="footer-bottom">
+                <p>&copy; 2024 COOK ME. All Rights Reserved.</p>
+            </div>
+    </footer>
+
+    <link rel="stylesheet" type="text/css" href="./CSS/footer.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"><!--icon link-->
+
+</body>
+</html>
